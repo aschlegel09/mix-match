112a408aa54a622fc4dec53aef2f16cb009e46e4
Andrew Schlegel <aschlegel09@gmail.com>
2018-12-27 09:48:10 -0500

    images footer fixed

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import "./Wrapper.css"
 
 const Wrapper = props =>
 
-    <div className="jumbotron main ">
+    <div className="main">
         <h1 className="head top-head">Mix &amp; Match Game!</h1>
         <br />
         <h4 className="head mb-4">Click on an image to earn points,<br /> but don't click on any one more than once!</h4>
-
-        <div className="jumbotron card-body">{props.children}</div>
+        <div className="wrapper">{props.children}</div>
         <div className="footer-copyright text-center py-3">© 2018 Copyright: <a href="https://github.com/aschlegel09/mix-match" className="text-white">aschlegel09 Github</a>
-        </div>
+      </div>
     </div>
 
 export default Wrapper;
--- src/components/cards.js
+++ src/components/cards.js
@@ -4,7 +4,7 @@ import "../App.css";
 const Card = props => (
     <div className="card-item" onClick={() => props.randomRender(props.id)}>
         <div className="img-container">
-            <img alt={props.name} src={props.image} />
+            <img alt={props.name} src={props.image} className="card-img" />
         </div>
     </div>
 )
